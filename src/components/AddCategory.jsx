abb383755dd16ae6d53becb08fae21afdb2bb386
Isabel Marín <icmarint@unal.edu.co>
2023-04-04 20:51:19 -0500

    Adding testing

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types'
 
 export const AddCategory = ({ onNewCategory }) => {
 
@@ -12,12 +13,12 @@ export const AddCategory = ({ onNewCategory }) => {
         event.preventDefault();
         if( inputValue.trim().length <= 1) return;
         /* setCategories(categories => [ inputValue, ...categories ]) */
-        onNewCategory( inputValue.trim());
         setInputValue('');
+        onNewCategory( inputValue.trim());
     }
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} aria-label="form">
             <input
                 type = "text"
                 placeholder = "Buscar"
@@ -27,4 +28,8 @@ export const AddCategory = ({ onNewCategory }) => {
         </form>
         
     )
+}
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
 }
